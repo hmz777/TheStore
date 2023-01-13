8a4a5231758cd03ec2badbfb3053fbe0d9faf372
HMZ <hamzi1995@gmail.com>
2023-01-14 01:27:22 +0300

    Create ISyncableAggregate to indicate of the decorated aggregate needs to be synced with other subdomains

---
+++ Shared/TheStore.SharedKernel/Interfaces/ISyncableAggregate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheStore.SharedKernel.Interfaces
+{
+	public interface ISyncableAggregate
+	{
+		public bool NeedsSynchronization { get; set; }
+	}
+}
