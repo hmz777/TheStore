31f3aad4f576322e7f985090ad0da89d0c456b20
HMZ <hamzi1995@gmail.com>
2023-04-06 02:15:48 +0300

    Extend the repository's functionality with ExecuteDelete

--- Src/Shared/TheStore.ApiCommon/Data/Repository/DataRepository.cs
+++ Src/Shared/TheStore.ApiCommon/Data/Repository/DataRepository.cs
@@ -1,14 +1,34 @@
-﻿using Ardalis.Specification.EntityFrameworkCore;
+﻿using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TheStore.SharedKernel.Entities;
 using TheStore.SharedKernel.Interfaces;
 
 namespace TheStore.ApiCommon.Data.Repository
 {
 	public class DataRepository<TContext, T> : RepositoryBase<T>, IApiRepository<TContext, T> where TContext : DbContext where T : class, IAggregateRoot
 	{
+		private readonly TContext context;
+
 		public DataRepository(TContext context) : base(context)
 		{
+			this.context = context;
+		}
 
+		public async Task ExecuteDeleteAsync(Specification<T> specification, CancellationToken cancellationToken = default)
+		{
+			await SpecificationEvaluator.Default.GetQuery(query: context.Set<T>(), specification).ExecuteDeleteAsync(cancellationToken);
+		}
+
+		public async Task ExecuteDeleteAsync<TEntity, TId>(TId id, CancellationToken cancellationToken = default)
+			where TId : notnull where TEntity : BaseEntity<TId>
+		{
+			await context.Set<TEntity>().Where(x => x.Id.Equals(id)).ExecuteDeleteAsync(cancellationToken);
+		}
+
+		public Task ExecuteUpdateAsync()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
--- Src/Shared/TheStore.ApiCommon/Data/Repository/IApiRepository.cs
+++ Src/Shared/TheStore.ApiCommon/Data/Repository/IApiRepository.cs
@@ -1,15 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ardalis.Specification;
+using Microsoft.EntityFrameworkCore;
 using TheStore.SharedKernel.Interfaces;
 
 namespace TheStore.ApiCommon.Data.Repository
 {
 	public interface IApiRepository<TContext, T> : IRepository<T> where TContext : DbContext where T : class, IAggregateRoot
 	{
-
+		public Task ExecuteUpdateAsync();
+		public Task ExecuteDeleteAsync(Specification<T> specification, CancellationToken cancellationToken = default);
+		public Task ExecuteDeleteAsync<TId>(T entity, CancellationToken cancellationToken = default) where TId : notnull;
 	}
 }
