4833c3a29093fd9a8767e0ecd5da48493aadb7cb
HMZ <hamzi1995@gmail.com>
2023-04-06 17:11:07 +0300

    Add property mapping helper

---
+++ Src/Shared/TheStore.ApiCommon/Data/Helpers/RepositoryHelpers.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using TheStore.ApiCommon.Data.Repository;
+using TheStore.SharedKernel.Interfaces;
+using TheStore.SharedModels.Models;
+
+namespace TheStore.ApiCommon.Data.Helpers
+{
+	public class RepositoryHelpers
+	{
+		public async static Task PropertyUpdateAsync<TViewModel, TEntity, TContext>(
+			TViewModel viewModel, TEntity entity, IMapper mapper, IApiRepository<TContext, TEntity> repository)
+			where TViewModel : RequestBase where TEntity : class, IAggregateRoot where TContext : DbContext
+		{
+			mapper.Map(viewModel, entity);
+			await repository.SaveChangesAsync();
+		}
+	}
+}
