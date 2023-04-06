94f2a2906a5a770a475b6483ca1b303432ad2dd1
HMZ <hamzi1995@gmail.com>
2023-04-06 17:06:10 +0300

    Fix an error in unit tests that didn't allow for autofixture to supply dummy file Uris

--- Tests/TheStore.UnitTests/AutoData/Specimens/FileUriSpecimen.cs
+++ Tests/TheStore.UnitTests/AutoData/Specimens/FileUriSpecimen.cs
@@ -9,9 +9,9 @@ namespace TheStore.Domain.Tests.AutoData.Specimens
         {
             return request switch
             {
-                ParameterInfo p when p.ParameterType == typeof(Uri) &&
-                p.Name!.Contains("File", StringComparison.InvariantCultureIgnoreCase) &&
-                p.Name.Contains("Uri", StringComparison.InvariantCultureIgnoreCase) => new Uri("http://example.com/file.txt"),
+                ParameterInfo p when p.ParameterType == typeof(string) &&
+                p.Name!.Contains("File", StringComparison.InvariantCultureIgnoreCase) ||
+                p.Name.Contains("Uri", StringComparison.InvariantCultureIgnoreCase) => "http://example.com/file.txt",
                 _ => new NoSpecimen()
             };
         }
