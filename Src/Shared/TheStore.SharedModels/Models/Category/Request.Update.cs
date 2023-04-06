20da181c6908046b522fdd62b611803abddb3f3a
HMZ <hamzi1995@gmail.com>
2023-04-06 17:11:26 +0300

    Annotate the update request

--- Src/Shared/TheStore.SharedModels/Models/Category/Request.Update.cs
+++ Src/Shared/TheStore.SharedModels/Models/Category/Request.Update.cs
@@ -1,13 +1,22 @@
-﻿namespace TheStore.SharedModels.Models.Category
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheStore.SharedModels.Models.Category
 {
 	public class UpdateRequest : RequestBase
 	{
 		public const string RouteTemplate = "categories/{CategoryId:int}";
 		public override string Route => RouteTemplate.Replace("{CategoryId:int}", CategoryId.ToString());
 
+		[FromRoute]
 		public int CategoryId { get; set; }
+
+		[FromBody]
 		public int Order { get; set; }
+
+		[FromBody]
 		public string Name { get; set; }
+
+		[FromBody]
 		public bool Active { get; set; }
 	}
 }
