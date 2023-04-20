88f420e74cc00d9bfedea3b509b168ddae979ab7
HMZ <hamzi1995@gmail.com>
2023-04-20 19:42:49 +0300

    Add integration tests

---
+++ Tests/TheStore.Endpoints.IntegrationTests/Categories/CategoriesSpec.cs
@@ -0,0 +1,74 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using TheStore.Endpoints.IntegrationTests.WebApplication;
+using TheStore.SharedModels.Models.Categories;
+
+namespace TheStore.Endpoints.IntegrationTests.Categories
+{
+	public class CategoriesSpec : IClassFixture<CustomWebApplicationFactory<Program>>
+	{
+		private readonly HttpClient _client;
+
+		public CategoriesSpec(CustomWebApplicationFactory<Program> factory)
+				=> _client = factory.CreateClient();
+
+		[Fact]
+		public async Task Can_List_Categories()
+		{
+			var request = new ListRequest(1, 10);
+
+			var response = await _client
+				.GetFromJsonAsync<List<CategoryDto>>(request.Route);
+
+			response.Should().NotBeNull();
+			response.Should().HaveCount(request.Take);
+		}
+
+		[Fact]
+		public async Task Can_Get_Category_By_Id()
+		{
+			var request = new GetByIdRequest(1);
+
+			var response = await _client
+				.GetFromJsonAsync<CategoryDto>(request.Route);
+
+			response.Should().NotBeNull();
+		}
+
+		[Fact]
+		public async Task Can_Create_Category()
+		{
+			var fixture = new Fixture();
+			var request = fixture.Create<CreateRequest>();
+
+			var response = await _client
+				.PostAsJsonAsync(CreateRequest.RouteTemplate, request);
+
+			((int)response.StatusCode).Should().Be(StatusCodes.Status201Created);
+			response.Headers.Location.Should().NotBeNull();
+		}
+
+		[Fact]
+		public async Task Can_Update_Category()
+		{
+			var fixture = new Fixture();
+			var request = fixture.Create<UpdateRequest>();
+			request.CategoryId = 1;
+
+			var response = await _client
+				.PutAsJsonAsync(request.Route, request);
+
+			((int)response.StatusCode).Should().Be(StatusCodes.Status204NoContent);
+		}
+
+		[Fact]
+		public async Task Can_Delete_Category()
+		{
+			var request = new DeleteRequest(20);
+
+			var response = await _client.DeleteAsync(request.Route);
+
+			((int)response.StatusCode).Should().Be(StatusCodes.Status204NoContent);
+		}
+	}
+}
---
+++ Tests/TheStore.Endpoints.IntegrationTests/Helpers/DockerSqlServerDatabaseHelper.cs
@@ -0,0 +1,111 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using Xunit.Sdk;
+
+namespace TheStore.Endpoints.IntegrationTests.Helpers
+{
+	public class DockerSqlServerDatabaseHelper : IAsyncDisposable
+	{
+		private readonly DockerClient dockerClient;
+		private string? containerId;
+		private string containerName;
+
+		public bool DatabaseServerStarted { get; private set; }
+		public int Port { get; private set; }
+		public string Password { get; private set; }
+
+		public DockerSqlServerDatabaseHelper()
+		{
+			dockerClient = new DockerClientConfiguration().CreateClient();
+
+			Port = 1433;
+			Password = "testpass@12345";
+			containerName = "SQLServer_Test";
+		}
+
+		public async Task StartDatabaseServer(CancellationToken cancellationToken = default)
+		{
+			if (await ContainerExistsAsync(containerName, cancellationToken) == false)
+			{
+				await dockerClient.Containers.CreateContainerAsync(new CreateContainerParameters()
+				{
+					Image = "mcr.microsoft.com/mssql/server:2019-latest",
+					Env = new List<string>()
+					{
+						"ACCEPT_EULA=Y",
+						$"MSSQL_SA_PASSWORD={Password}"
+					},
+					HostConfig = new HostConfig
+					{
+						PortBindings = new Dictionary<string, IList<PortBinding>>
+					{
+						{
+							"1433/tcp",
+							new List<PortBinding>
+							{
+								new PortBinding
+								{
+									HostPort = $"{Port}"
+								}
+							}
+						}
+					},
+					},
+					ExposedPorts = new Dictionary<string, EmptyStruct>
+					{
+						[$"{Port}/tcp"] = new EmptyStruct()
+					},
+					Name = containerName
+				}, cancellationToken);
+			}
+
+			await SetContainerIdAsync(containerName, cancellationToken);
+
+			await dockerClient.Containers.StartContainerAsync(containerId, new ContainerStartParameters(), cancellationToken);
+
+			DatabaseServerStarted = true;
+
+			// Wait for the container to start
+			await Task.Delay(3000, cancellationToken);
+		}
+
+		public async Task StopDatabaseServer(CancellationToken cancellationToken = default)
+		{
+			if (DatabaseServerStarted == false)
+				return;
+
+			await dockerClient.Containers.StopContainerAsync(containerId, new ContainerStopParameters
+			{
+				WaitBeforeKillSeconds = 10
+			}, cancellationToken);
+		}
+
+		private async Task<bool> ContainerExistsAsync(string containerName, CancellationToken cancellationToken = default)
+		{
+			var containers = await dockerClient.Containers.ListContainersAsync(new ContainersListParameters()
+			{
+				All = true
+			}, cancellationToken);
+
+			return containers.Any(c => c.Names.Contains("/" + containerName));
+		}
+
+		private async Task SetContainerIdAsync(string containerName, CancellationToken cancellationToken = default)
+		{
+			var containers = await dockerClient.Containers.ListContainersAsync(new ContainersListParameters()
+			{
+				All = true
+			}, cancellationToken);
+
+			containerId = containers.First(c => c.Names.Contains("/" + containerName)).ID;
+		}
+
+		public async ValueTask DisposeAsync()
+		{
+			await dockerClient.Containers.StopContainerAsync(containerName, new ContainerStopParameters());
+			await dockerClient.Containers.RemoveContainerAsync(containerName,
+				new ContainerRemoveParameters() { Force = true });
+			dockerClient.Dispose();
+		}
+	}
+}
