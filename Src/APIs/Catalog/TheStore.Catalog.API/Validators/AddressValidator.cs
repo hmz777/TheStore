4960cd356c091a22122a7acdc304fc23508cc742
HMZ <hamzi1995@gmail.com>
2023-05-04 22:20:39 +0300

    Add Address validator

---
+++ Src/APIs/Catalog/TheStore.Catalog.API/Validators/AddressValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using TheStore.SharedModels.Models.ValueObjectsDtos;
+
+namespace TheStore.Catalog.API.Validators
+{
+	public class AddressValidator : AbstractValidator<AddressDto>
+	{
+		public AddressValidator()
+		{
+
+		}
+	}
+}
