adc51d850097bc8d1269a2e46288e810d06adfb2
HMZ <hamzi1995@gmail.com>
2023-06-14 00:26:38 +0300

    Rename T to TContext for clarity

--- Src/Shared/TheStore.ApiCommon/Interfaces/IDataSeeder.cs
+++ Src/Shared/TheStore.ApiCommon/Interfaces/IDataSeeder.cs
@@ -2,8 +2,8 @@
 
 namespace TheStore.ApiCommon.Interfaces
 {
-	public interface IDataSeeder<T> where T : DbContext
+	public interface IDataSeeder<TContext> where TContext : DbContext
 	{
-		public Task SeedDataAsync(T context);
+		public Task SeedDataAsync(TContext context);
 	}
 }
