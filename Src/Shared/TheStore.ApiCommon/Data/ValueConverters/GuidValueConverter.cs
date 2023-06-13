185ad7b5836717efeac1cce7605ae4be08a023e9
HMZ <hamzi1995@gmail.com>
2023-06-14 00:24:53 +0300

    Add value converter for Guids

---
+++ Src/Shared/TheStore.ApiCommon/Data/ValueConverters/GuidValueConverter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace TheStore.ApiCommon.Data.ValueConverters
+{
+	public class GuidValueConverter : ValueConverter<Guid, string>
+	{
+		public GuidValueConverter() : base(guid => guid.ToString(), guidStr => Guid.Parse(guidStr)) { }
+	}
+}
