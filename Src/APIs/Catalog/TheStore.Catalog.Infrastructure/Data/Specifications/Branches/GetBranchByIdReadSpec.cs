19a9f32e6673ff507aa2e2c202d895c108b192fe
HMZ <hamzi1995@gmail.com>
2023-06-15 17:18:35 +0300

    Add the new spec library changes

--- Src/APIs/Catalog/TheStore.Catalog.Infrastructure/Data/Specifications/Branches/GetBranchByIdReadSpec.cs
+++ Src/APIs/Catalog/TheStore.Catalog.Infrastructure/Data/Specifications/Branches/GetBranchByIdReadSpec.cs
@@ -3,7 +3,8 @@ using TheStore.Catalog.Core.Aggregates.Branches;
 
 namespace TheStore.Catalog.Infrastructure.Data.Specifications.Branches
 {
-	public class GetBranchByIdReadSpec : Specification<Branch>, ISingleResultSpecification
+	public class GetBranchByIdReadSpec : Specification<Branch>,
+		ISingleResultSpecification<Branch>
 	{
 		public GetBranchByIdReadSpec(int branchId)
 		{
--- Src/APIs/Catalog/TheStore.Catalog.Infrastructure/Data/Specifications/Categories/GetCategoryByIdReadSpec.cs
+++ Src/APIs/Catalog/TheStore.Catalog.Infrastructure/Data/Specifications/Categories/GetCategoryByIdReadSpec.cs
@@ -4,7 +4,8 @@ using TheStore.Catalog.Core.ValueObjects.Keys;
 
 namespace TheStore.Catalog.Infrastructure.Data.Specifications.Categories
 {
-	public class GetCategoryByIdReadSpec : Specification<Category>, ISingleResultSpecification
+	public class GetCategoryByIdReadSpec : Specification<Category>,
+		ISingleResultSpecification<Category>
 	{
 		public GetCategoryByIdReadSpec(CategoryId categoryId)
 		{
