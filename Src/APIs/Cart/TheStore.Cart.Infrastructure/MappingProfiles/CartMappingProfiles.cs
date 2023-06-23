e51d7cdce75010394bb360c440dd23e8e6a937c5
HMZ <hamzi1995@gmail.com>
2023-06-23 13:27:57 +0300

    Configure AutoMapper mappings for the cart endpoints

---
+++ Src/APIs/Cart/TheStore.Cart.Infrastructure/MappingProfiles/CartMappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using TheStore.Cart.Core.Aggregates;
+using TheStore.Cart.Core.Entities;
+using TheStore.SharedModels.Models.Cart;
+using TheStore.SharedModels.Models.Wishlist;
+
+namespace TheStore.Cart.Infrastructure.MappingProfiles
+{
+	public class CartMappingProfiles : Profile
+	{
+		public CartMappingProfiles()
+		{
+			CreateMap<CartItem, CartItemDto>();
+			CreateMap<Core.Aggregates.Cart, CartDto>()
+				.ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items.ToList()));
+
+			CreateMap<WishlistItem, WishlistItemDto>();
+			CreateMap<Wishlist, WishlistDto>()
+				.ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items.ToList()));
+		}
+	}
+}
