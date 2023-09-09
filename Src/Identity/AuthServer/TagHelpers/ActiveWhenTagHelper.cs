785c35cc4c9ec25c18ac42abc28ab6fcdbe4575b
HMZ <hamzi1995@gmail.com>
2023-09-10 02:21:15 +0300

    Create tag helper to highlight navigation list items on selection based on path

---
+++ Src/Identity/AuthServer/TagHelpers/ActiveWhenTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+
+namespace AuthServer.TagHelpers
+{
+	[HtmlTargetElement("li")]
+	public class ActiveWhenTagHelper : TagHelper
+	{
+		[ViewContext]
+		[HtmlAttributeNotBound]
+		public ViewContext ViewContext { get; set; }
+
+		public string ActiveWhen { get; set; }
+		public string ActiveClass { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{		
+			var request = ViewContext.HttpContext.Request;
+
+			if (request.Path.ToString().Contains(ActiveWhen, StringComparison.InvariantCultureIgnoreCase))
+			{
+				output.AddClass(ActiveClass, HtmlEncoder.Default);
+			}
+		}
+	}
+}
