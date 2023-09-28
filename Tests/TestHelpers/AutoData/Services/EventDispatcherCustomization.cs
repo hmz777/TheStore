2783b1da09c60ee2e02f39a7ac8c0cfb5d35151c
HMZ <hamzi1995@gmail.com>
2023-09-28 15:56:00 +0300

    Add a test customization for the event dispatcher

---
+++ Tests/TestHelpers/AutoData/Services/EventDispatcherCustomization.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture;
+using MassTransit;
+using MassTransit.Mediator;
+using Moq;
+using TheStore.ApiCommon.Services;
+using TheStore.Events;
+
+namespace TheStore.TestHelpers.AutoData.Services
+{
+	public class EventDispatcherCustomization : ICustomization
+	{
+		public void Customize(IFixture fixture)
+		{
+			fixture.Register(() =>
+			{
+				var mediator = new Mock<IMediator>();
+				mediator.Setup(x => x.Publish(It.IsAny<IEvent>(), default))
+						.Returns(Task.CompletedTask);
+
+				var bus = new Mock<IBus>();
+				bus.Setup(x => x.Publish(It.IsAny<IEvent>(), default))
+					.Returns(Task.CompletedTask);
+
+				return new EventDispatcher(mediator.Object, bus.Object);
+			});
+		}
+	}
+}
