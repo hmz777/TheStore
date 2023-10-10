56becc2798939c397d518aafe01e627884f12aff
HMZ <hamzi1995@gmail.com>
2023-10-10 04:13:06 +0300

    Cleanup

--- Src/Shared/TheStore.SharedKernel/DomainEvents/BaseDomainEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace TheStore.SharedKernel.DomainEvents
-{
-	public abstract class BaseDomainEvent : INotification
-	{
-		public DateTimeOffset DateOccurred { get; protected set; } = DateTimeOffset.UtcNow;
-	}
-}
--- Src/Shared/TheStore.SharedKernel/Interfaces/IHandle.cs
+++
@@ -1,9 +0,0 @@
-﻿using TheStore.SharedKernel.DomainEvents;
-
-namespace TheStore.SharedKernel.Interfaces
-{
-	public interface IHandle<T> where T : BaseDomainEvent
-	{
-		Task HandleAsync(T args);
-	}
-}
