aa3997e90ad20fd505e8a5eb985fe8b2cfb175ce
HMZ <hamzi1995@gmail.com>
2023-10-25 19:47:35 +0300

    Add equality helpers

---
+++ Src/Web/TheStore.Web.Blazor/Helpers/EqualityHelpers.cs
@@ -0,0 +1,7 @@
+﻿namespace TheStore.Web.Blazor.Helpers
+{
+	public static class EqualityHelpers
+	{
+		public static bool AreEqual<T>(T? left, T? right) => EqualityComparer<T>.Default.Equals(left, right);
+	}
+}
