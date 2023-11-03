8b9cfb82ff1f7632b6de8bdec06d1cf59c7b3bf4
HMZ <hamzi1995@gmail.com>
2023-11-03 21:27:09 +0200

    Introduce a dummy data helper for easily populating dummy data

---
+++ Src/APIs/Catalog/TheStore.Catalog.Infrastructure/Data/DummyDataHelper.cs
@@ -0,0 +1,119 @@
+﻿using TheStore.Catalog.Core.Aggregates.Branches;
+using TheStore.Catalog.Core.Aggregates.Categories;
+using TheStore.Catalog.Core.Aggregates.Products;
+using TheStore.Catalog.Core.ValueObjects;
+using TheStore.Catalog.Core.ValueObjects.Keys;
+using TheStore.Catalog.Core.ValueObjects.Products;
+
+namespace TheStore.Catalog.Infrastructure.Data
+{
+	public class DummyDataHelper
+	{
+		private static readonly string stringData =
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+		public static string GenerateRandomString()
+		{
+			var rand = new Random();
+			return stringData[..rand.Next(15, stringData.Length)];
+		}
+
+		public static string GenerateRandomSmallString()
+		{
+			var rand = new Random();
+			return stringData[..rand.Next(15, 100)];
+		}
+
+		public static string GenerateRandomHexColor()
+		{
+			var random = new Random();
+			return string.Format("#{0:X6}", random.Next(0x1000000));
+		}
+
+		public static List<Category> GenerateDummyCategories()
+		{
+			var categories = new List<Category>();
+
+			for (int i = 0; i < 20; i++)
+			{
+				var category = new Category(i + 1, $"Category {i + 1}", true);
+
+				categories.Add(category);
+			}
+
+			return categories;
+		}
+
+		public static List<Branch> GenerateDummyBranches()
+		{
+			var branches = new List<Branch>();
+
+			for (int i = 0; i < 20; i++)
+			{
+				var branch = new Branch(
+					$"Branch {i + 1}",
+					GenerateRandomString(),
+					new Address("Syria", "Tartus", "Barranieh", $"ZIP{i}", new Coordinate(0, 0)),
+					new Image($"https://placehold.co/600x400.png", "Placeholder image", false));
+
+				branches.Add(branch);
+			}
+
+			return branches;
+		}
+
+		public static List<Product> GenerateDummyProducts()
+		{
+			var rand = new Random();
+			var randomNumeber = rand.Next(5, 5000);
+			var randomProductNumber = rand.Next(1, 100);
+			var randomVariantNumber = rand.Next(1, 5);
+			var randomImageNumber = rand.Next(1, 10);
+
+			var products = new List<Product>();
+
+			for (int i = 0; i < randomProductNumber; i++)
+			{
+				var variants = new List<ProductVariant>();
+
+				for (int j = 0; j < randomVariantNumber; j++)
+				{
+					var color = GenerateRandomHexColor();
+
+					var images = new List<Image>();
+
+					for (int m = 0; m < randomImageNumber; m++)
+					{
+						var imageColor = color.Replace("#", "");
+
+						images.Add(
+							new Image($"https://placehold.co/600x400/{imageColor}/{(imageColor == "FFFFFF" ? "000000" : "FFFFFF")}/png?text=Image+{j}",
+							GenerateRandomSmallString(),
+							false));
+					}
+
+					var variant = new ProductVariant(
+								Guid.NewGuid(),
+								GenerateRandomString(),
+								GenerateRandomString(),
+								new Money((j + 1) * 500, Currency.Usd),
+								new InventoryRecord((i + 1) * 2, 5, 100, 0, false),
+								new ProductColor(color, false, images),
+								new Dimentions(randomNumeber, randomNumeber, randomNumeber, UnitOfMeasure.Cm),
+								new ProductSpecifications(new Dictionary<string, string>() { { "Name", "Value" } }));
+
+					variants.Add(variant);
+				}
+
+				var product = new Product(
+								new CategoryId(i + 1),
+								GenerateRandomSmallString(),
+								variants);
+
+				products.Add(product);
+			}
+
+			return products;
+		}
+	}
+}
