7d5e198734d3399588d68632b0ad065116f9257d
HMZ <hamzi1995@gmail.com>
2023-11-03 21:16:04 +0200

    Add mutlilanguage strings instead of strings

--- Src/APIs/Catalog/TheStore.Catalog.Core/Aggregates/Branches/Branch.cs
+++ Src/APIs/Catalog/TheStore.Catalog.Core/Aggregates/Branches/Branch.cs
@@ -2,44 +2,31 @@
 using TheStore.Catalog.Core.ValueObjects;
 using TheStore.SharedKernel.Entities;
 using TheStore.SharedKernel.Interfaces;
+using TheStore.SharedKernel.ValueObjects;
 
 namespace TheStore.Catalog.Core.Aggregates.Branches
 {
 	public class Branch : BaseEntity<int>, IAggregateRoot
 	{
-		public string Name { get; set; }
-		public string Description { get; set; }
+		public MultilanguageString Name { get; set; }
+		public MultilanguageString Description { get; set; }
 		public Address Address { get; set; }
-		public Image? Image { get; set; }
+		public Image Image { get; set; }
+		public bool Published { get; set; }
 
 		// Ef Core
-		private Branch()
-		{
+		private Branch() { }
 
-		}
-
-		public Branch(string name, string description, Address address)
+		public Branch(MultilanguageString name, MultilanguageString description, Address address, bool published)
 		{
-			Guard.Against.NullOrEmpty(name, nameof(name));
-			Guard.Against.NullOrEmpty(description, nameof(description));
+			Guard.Against.Null(name, nameof(name));
+			Guard.Against.Null(description, nameof(description));
 			Guard.Against.Null(address, nameof(address));
 
 			Name = name;
 			Description = description;
 			Address = address;
-		}
-
-		public Branch(string name, string description, Address address, Image image)
-		{
-			Guard.Against.NullOrEmpty(name, nameof(name));
-			Guard.Against.NullOrEmpty(description, nameof(description));
-			Guard.Against.Null(address, nameof(address));
-			Guard.Against.Null(image, nameof(image));
-
-			Name = name;
-			Description = description;
-			Address = address;
-			Image = image;
+			Published = published;
 		}
 	}
 }
--- Src/APIs/Catalog/TheStore.Catalog.Core/Aggregates/Categories/Category.cs
+++ Src/APIs/Catalog/TheStore.Catalog.Core/Aggregates/Categories/Category.cs
@@ -2,29 +2,28 @@
 using TheStore.Catalog.Core.ValueObjects.Keys;
 using TheStore.SharedKernel.Entities;
 using TheStore.SharedKernel.Interfaces;
+using TheStore.SharedKernel.ValueObjects;
 
 namespace TheStore.Catalog.Core.Aggregates.Categories
 {
 	public class Category : BaseEntity<CategoryId>, IAggregateRoot, ISyncableAggregate
 	{
 		public int Order { get; set; }
-		public string Name { get; set; }
-		public bool Active { get; set; }
+		public MultilanguageString Name { get; set; }
+		public bool Published { get; set; }
 		public bool NeedsSynchronization { get; set; }
 
-		private Category()
-		{
+		// EF Core
+		private Category() { }
 
-		}
-
-		public Category(int order, string name, bool active)
+		public Category(int order, MultilanguageString name, bool published)
 		{
 			Guard.Against.NegativeOrZero(order, nameof(order));
-			Guard.Against.NullOrEmpty(name, nameof(name));
+			Guard.Against.Null(name, nameof(name));
 
 			Order = order;
 			Name = name;
-			Active = active;
+			Published = published;
 		}
 	}
 }
