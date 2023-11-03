498bd062beee6fe9cac4d8682957e2f0a3e9cc5e
HMZ <hamzi1995@gmail.com>
2023-11-03 21:23:02 +0200

    Update ProductColor

--- Src/APIs/Catalog/TheStore.Catalog.Core/ValueObjects/Products/ProductColor.cs
+++ Src/APIs/Catalog/TheStore.Catalog.Core/ValueObjects/Products/ProductColor.cs
@@ -7,25 +7,27 @@ namespace TheStore.Catalog.Core.ValueObjects.Products
 {
 	public class ProductColor : ValueObject
 	{
-		public string ColorCode { get; private set; }
+		private readonly List<Image> images = new();
 
-		private List<Image> images = new();
+		public string ColorName { get; }
+		public string ColorCode { get; }
+		public bool IsMainColor { get; }
 
 		[NotMapped]
 		public ReadOnlyCollection<Image> Images => images.AsReadOnly();
 
 		// Ef Core
-		private ProductColor()
-		{
+		private ProductColor() { }
 
-		}
-
-		public ProductColor(string colorCode, List<Image> images)
+		public ProductColor(string colorName, string colorCode, bool isMainColor, List<Image> images)
 		{
-			Guard.Against.NullOrWhiteSpace(colorCode, nameof(colorCode));
+			Guard.Against.NullOrEmpty(colorName, nameof(colorName));
+			Guard.Against.NullOrEmpty(colorCode, nameof(colorCode));
 			Guard.Against.InvalidFormat(colorCode, nameof(colorCode), regexPattern: "^(?:[0-9a-fA-F]{3,4}){1,2}$");
 
+			ColorName = colorName;
 			ColorCode = colorCode;
+			IsMainColor = isMainColor;
 			this.images = images ?? new List<Image>();
 		}
 
@@ -38,7 +40,7 @@ namespace TheStore.Catalog.Core.ValueObjects.Products
 				image
 			};
 
-			return new ProductColor(ColorCode, newImages);
+			return new ProductColor(ColorCode, IsMainColor, newImages);
 		}
 
 		public ProductColor RemoveImage(Image image)
@@ -48,7 +50,7 @@ namespace TheStore.Catalog.Core.ValueObjects.Products
 			var newImages = new List<Image>(images);
 			newImages.Remove(image);
 
-			return new ProductColor(ColorCode, newImages);
+			return new ProductColor(ColorCode, IsMainColor, newImages);
 		}
 
 		protected override IEnumerable<IComparable> GetEqualityComponents()
