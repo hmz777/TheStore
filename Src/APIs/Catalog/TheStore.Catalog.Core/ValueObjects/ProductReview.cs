536f4002eca6ef798e9f9977ba31646f805182e3
HMZ <hamzi1995@gmail.com>
2023-11-14 14:37:50 +0200

    Update property access in ProductReview value object

--- Src/APIs/Catalog/TheStore.Catalog.Core/ValueObjects/ProductReview.cs
+++ Src/APIs/Catalog/TheStore.Catalog.Core/ValueObjects/ProductReview.cs
@@ -5,11 +5,11 @@ namespace TheStore.Catalog.Core.ValueObjects
 {
 	public class ProductReview : ValueObject
 	{
-		public string Title { get; set; }
-		public DateTimeOffset Date { get; set; }
-		public string Content { get; set; }
-		public int Rating { get; set; }
-		public string User { get; set; }
+		public string Title { get; }
+		public DateTimeOffset Date { get; }
+		public string Content { get; }
+		public int Rating { get; }
+		public string User { get; }
 
 		public ProductReview(string title, DateTimeOffset date, string content, int rating, string user)
 		{
