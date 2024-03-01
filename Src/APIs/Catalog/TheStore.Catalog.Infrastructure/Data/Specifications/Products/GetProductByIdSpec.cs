227044ce2301fd795ae5f15e5d36771084864ed0
HMZ <hamzi1995@gmail.com>
2024-03-01 21:31:04 +0100

    Add Get Product by Id spec

---
+++ Src/APIs/Catalog/TheStore.Catalog.Infrastructure/Data/Specifications/Products/GetProductByIdSpec.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Specification;
+using TheStore.Catalog.Core.Aggregates.Products;
+using TheStore.Catalog.Core.ValueObjects.Keys;
+
+namespace TheStore.Catalog.Infrastructure.Data.Specifications.Products
+{
+	public class GetProductByIdSpec : Specification<Product>, ISingleResultSpecification<Product>
+	{
+		public GetProductByIdSpec(ProductId productId)
+		{
+			Query.Where(product => product.Id == productId);
+		}
+	}
+}
