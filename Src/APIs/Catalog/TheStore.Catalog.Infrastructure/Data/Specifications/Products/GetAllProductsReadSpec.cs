93c5476b9bbea3f7d9a8987d6dc3448eb74209cf
HMZ <hamzi1995@gmail.com>
2024-03-01 21:30:38 +0100

    Add Get All Products readonly spec

---
+++ Src/APIs/Catalog/TheStore.Catalog.Infrastructure/Data/Specifications/Products/GetAllProductsReadSpec.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Specification;
+using TheStore.Catalog.Core.Aggregates.Products;
+
+namespace TheStore.Catalog.Infrastructure.Data.Specifications.Products
+{
+	public class GetAllProductsReadSpec : Specification<Product>
+	{
+		public GetAllProductsReadSpec()
+		{
+			Query
+				.AsNoTracking();
+		}
+	}
+}
