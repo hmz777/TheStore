cadf3e8790c1520150fc1b113f7ce7746da61d51
Hamzi Alsheikh <hamzi1995@gmail.com>
2024-06-29 01:28:17 +0200

    Move ProductSpecification to the aggregates folder

---
+++ Src/APIs/Catalog/TheStore.Catalog.Core/Aggregates/Products/ProductSpecification.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.GuardClauses;
+using TheStore.SharedKernel.ValueObjects;
+
+namespace TheStore.Catalog.Core.Aggregates.Products
+{
+    public class ProductSpecification
+    {
+        public MultilanguageString Name { get; set; }
+        public MultilanguageString Value { get; set; }
+
+        // Ef Core
+        private ProductSpecification() { }
+
+        public ProductSpecification(MultilanguageString name, MultilanguageString value)
+        {
+            Guard.Against.Null(name, nameof(name));
+            Guard.Against.Null(value, nameof(value));
+
+            Name = name;
+            Value = value;
+        }
+    }
+}
