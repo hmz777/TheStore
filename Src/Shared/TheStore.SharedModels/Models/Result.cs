396af6197e82d88b6194b01c84add9fd0e5c012e
Hamzi Alsheikh <hamzi1995@gmail.com>
2024-07-26 14:39:10 +0200

    Add a generic variant of Result in order to return a value as well

--- Src/Shared/TheStore.SharedModels/Models/Result.cs
+++ Src/Shared/TheStore.SharedModels/Models/Result.cs
@@ -1,15 +1,31 @@
 ﻿
 namespace TheStore.SharedModels.Models
 {
-	public class Result
-	{
-		public Status Status { get; }
-		public string Message { get; }
+    public class Result<T>
+    {
+        public T Value { get; }
+        public Status Status { get; }
+        public string Message { get; }
+        public bool IsSuccessful => Status == Status.Success;
 
-		public Result(Status status, string message)
-		{
-			Status = status;
-			Message = message ?? throw new ArgumentNullException(nameof(message));
-		}
-	}
+        public Result(T value, Status status, string message)
+        {
+            Value = value;
+            Status = status;
+            Message = message ?? throw new ArgumentNullException(nameof(message));
+        }
+    }
+
+    public class Result
+    {
+        public Status Status { get; }
+        public string Message { get; }
+        public bool IsSuccessful => Status == Status.Success;
+
+        public Result(Status status, string message)
+        {
+            Status = status;
+            Message = message ?? throw new ArgumentNullException(nameof(message));
+        }
+    }
 }
