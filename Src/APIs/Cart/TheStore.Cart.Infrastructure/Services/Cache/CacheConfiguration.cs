af26eb304eb3a801d3e24b5aa3258322de7a11c6
Hamzi Alsheikh <hamzi1995@gmail.com>
2024-08-07 15:50:03 +0200

    Implement the ICacheConfiguration interface in the Cart microservice

---
+++ Src/APIs/Cart/TheStore.Cart.Infrastructure/Services/Cache/CacheConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using TheStore.ApiCommon.Interfaces;
+
+namespace TheStore.Cart.Infrastructure.Services.Cache
+{
+    public class CacheConfiguration : ICacheConfiguration
+    {
+        public const string CacheConfigurationConfigKey = nameof(CacheConfiguration);
+
+        public bool MemoryCacheEnabled { get; set; }
+        public int MemoryCacheAbsoluteExpirationRelativeToNowInSeconds { get; set; }
+        public int MemoryCacheSlidingExpirationInSeconds { get; set; }
+    }
+}
