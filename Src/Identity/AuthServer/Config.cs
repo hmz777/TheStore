dffffb518da8fc13326e9f11baf831d690b80d77
Hamzi Alsheikh <hamzi1995@gmail.com>
2024-08-07 16:20:59 +0200

    Add static scopes for now

--- Src/Identity/AuthServer/Config.cs
+++ Src/Identity/AuthServer/Config.cs
@@ -21,10 +21,15 @@ namespace AuthServer
                 }
             ];
 
-        public static IEnumerable<ApiScope> ApiScopes => [];
+        public static IEnumerable<ApiScope> ApiScopes => [
+            new ApiScope("Api.Catalog.User.Read", "Read from Catalog API as User"),
+            new ApiScope("Api.Cart.User.Read", "Read from Cart API as User"),
+            new ApiScope("Api.Cart.User.Write", "Write to Cart API as User"),
+        ];
 
         public static IEnumerable<ApiResource> ApiResources => [];
 
+        // TODO: Reconfigure later for refresh tokens and dynamic scopes and permissions
         public static IEnumerable<Client> Clients =>
             [
                 // Interactive client using code flow + PKCE
@@ -43,7 +48,7 @@ namespace AuthServer
                     PostLogoutRedirectUris = {
                         "https://localhost:7676/signout-callback-oidc"
                     },
-                    AllowedScopes = { "openid", "profile" },
+                    AllowedScopes = { "openid", "profile", "Api.Catalog.User.Read", "Api.Cart.User.Read", "Api.Cart.User.Write" },
                     RequireConsent = false,
                     AllowOfflineAccess = false,
                     AlwaysIncludeUserClaimsInIdToken = true
---
+++ Src/Shared/TheStore.SharedModels/Claims/DefaultAppScopes.cs
@@ -0,0 +1,14 @@
+﻿namespace TheStore.SharedModels.Claims
+{
+    public static class DefaultAppScopes
+    {
+        // TODO: Later we'll seed those as default scopes but manage them dynamically via a CRUD API
+
+        public const string CatalogUserRead = "Api.Catalog.User.Read";
+        public const string CatalogUserWrite = "Api.Catalog.User.Write";
+        public const string CartUserRead = "Api.Cart.User.Read";
+        public const string CartUserWrite = "Api.Cart.User.Write";
+
+        public static string[] GetDefaultUserScopes() => new string[] { CatalogUserRead, CartUserRead, CartUserWrite };
+    }
+}
