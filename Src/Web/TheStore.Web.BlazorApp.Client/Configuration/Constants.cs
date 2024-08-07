266f48dc1dadbb8178db29178f3da13c77cf9d40
Hamzi Alsheikh <hamzi1995@gmail.com>
2024-08-07 16:12:04 +0200

    Add the names of HTTP clients as constants

---
+++ Src/Web/TheStore.Web.BlazorApp.Client/Configuration/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace TheStore.Web.BlazorApp.Client.Configuration
+{
+    public static class Constants
+    {
+        public static class HttpClientConstants
+        {
+            public const string ServerCatalogHttpClient = "ServerCatalogHttpClient";
+            public const string ServerAuthenticatedCartHttpClient = "ServerAuthenticatedCartHttpClient";
+            public const string ClientBackendHttpClient = "ClientBackendHttpClient";
+        }
+    }
+}
