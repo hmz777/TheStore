d7c2f825572782f347741368ff9844c4cb35860d
Hamzi Alsheikh <hamzi1995@gmail.com>
2024-08-07 16:07:46 +0200

    Move the constants for endpoint recognition in dev appsettings to the config class

--- Src/Web/TheStore.Web.BlazorApp.Client/Configuration/Endpoint.cs
+++ Src/Web/TheStore.Web.BlazorApp.Client/Configuration/Endpoint.cs
@@ -1,8 +1,8 @@
 ﻿namespace TheStore.Web.BlazorApp.Client.Configuration
 {
-	public class Endpoint
-	{
-		public string Name { get; set; }
-		public string Url { get; set; }
-	}
+    public class Endpoint
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
 }
---
+++ Src/Web/TheStore.Web.BlazorApp.Client/Configuration/EndpointsConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace TheStore.Web.BlazorApp.Client.Configuration
+{
+    public class EndpointsConfig
+    {
+        public const string Key = "Apis";
+        public const string CatalogEndpointKey = "Catalog Api";
+        public const string CartEndpointKey = "Cart Api";
+        public List<Endpoint> Endpoints { get; set; } = [];
+
+        public Endpoint GetCatalogEndpoint() => Endpoints.First(endpoint => endpoint.Name == CatalogEndpointKey);
+        public Endpoint GetCartEndpoint() => Endpoints.First(endpoint => endpoint.Name == CartEndpointKey);
+    }
+}
