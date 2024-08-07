4f2080f746f8a8a7ff17f0ea88c7242e44a16c2d
Hamzi Alsheikh <hamzi1995@gmail.com>
2024-08-07 16:05:55 +0200

    Rename Services to ServiceExtensions

---
+++ Src/Web/TheStore.Web.BlazorApp.Client/Extensions/ServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Serilog;
+using TheStore.Web.BlazorApp.Client.Auth;
+using TheStore.Web.BlazorApp.Client.Configuration;
+
+namespace TheStore.Web.BlazorApp.Client.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection ConfigureClientAuthenticationAndAuthorization(this IServiceCollection services)
+        {
+            Log.Information("Configure authorization");
+
+            services.AddAuthorizationCore();
+            services.AddCascadingAuthenticationState();
+            services.AddScoped<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureClientHttpClients(this IServiceCollection services, string baseAddress)
+        {
+            Log.Information("Configure http client");
+
+            services.AddTransient<AntiforgeryHandler>();
+            services.AddHttpClient(Constants.HttpClientConstants.ClientBackendHttpClient,
+                client => client.BaseAddress = new Uri(baseAddress))
+                    .AddHttpMessageHandler<AntiforgeryHandler>();
+
+            services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(Constants.HttpClientConstants.ClientBackendHttpClient));
+
+            return services;
+        }
+    }
+}
