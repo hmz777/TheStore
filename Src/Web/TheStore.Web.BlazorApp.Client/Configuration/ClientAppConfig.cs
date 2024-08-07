26d80907ffac6c9adcf2dc673de284e942a024ab
Hamzi Alsheikh <hamzi1995@gmail.com>
2024-08-07 16:13:06 +0200

    Remove client app config as it's not needed

--- Src/Web/TheStore.Web.BlazorApp.Client/Configuration/ClientAppConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TheStore.Web.BlazorApp.Client.Configuration
-{
-	public class ClientAppConfig
-	{
-		public const string Key = "Apis";
-		public List<Endpoint> Endpoints { get; set; } = [];
-	}
-}
