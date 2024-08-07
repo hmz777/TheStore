9a5c7d63bfc243f557a1f6543c4b1ad530439608
Hamzi Alsheikh <hamzi1995@gmail.com>
2024-08-07 16:04:06 +0200

    Add HttpContext extensions to easily acquire auth properties and access tokens

---
+++ Src/Web/TheStore.Web.BlazorApp/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace TheStore.Web.BlazorApp.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static AuthenticationProperties? GetAuthenticationProperties(this HttpContext httpContext)
+        {
+            return httpContext.Features.Get<IAuthenticateResultFeature>()?.AuthenticateResult?.Properties;
+        }
+
+        public static async Task<string?> GetAccessToken(this HttpContext httpContext)
+        {
+            return await httpContext.GetTokenAsync("access_token");
+        }
+    }
+}
